e80d580560e9bc27d7881072a8d18ee89bf0d690
Agile <Agile@AGILEWORX2>
2016-08-09 16:50:34 +0600

    test

--- PostgreSQL_App/Form1.Designer.cs
+++ PostgreSQL_App/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(338, 53);
+            this.button1.Location = new System.Drawing.Point(338, 98);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 51);
             this.button1.TabIndex = 0;
